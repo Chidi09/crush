3f64c83195ed7445a52883da855cda39ae8a8757
chidi09 <chidiisking7@gmail.com>
2026-06-14 23:40:47 +0000

    feat(web): new orange-whale favicons + OG social card
    - Swap favicon set (ico/16/32/apple-touch/android-chrome) to the new
      orange-circle whale; drop stale favicon.svg (old logo) and point both
      index.html files at the raster set so the new icon actually renders.
    - Add 1200x630 og-image.png (was 404) and wire absolute og:image /
      twitter:image in root + src index.html with width/height hints.
    - release.data.ts: pick up v0.8.4 Windows .exe/.msi download links.

--- crush-web/src/app/components/download-block/release.data.ts
+++ crush-web/src/app/components/download-block/release.data.ts
@@ -3,31 +3,41 @@ export interface ReleaseAsset { name: string; url: string; size: number; }
 export interface ReleaseData { version: string; assets: ReleaseAsset[]; stars: number; }
 
 export const RELEASE: ReleaseData = {
-  "version": "v0.8.4",
+  "version": "v0.8.5",
   "assets": [
     {
-      "name": "crush-0.8.4-windows-x86_64.exe",
-      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.4/crush-0.8.4-windows-x86_64.exe",
-      "size": 11282432
+      "name": "crush-0.8.5-windows-x86_64.exe",
+      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.5/crush-0.8.5-windows-x86_64.exe",
+      "size": 11182080
     },
     {
-      "name": "crush-v0.8.4-linux-x86_64",
-      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.4/crush-v0.8.4-linux-x86_64",
-      "size": 11420728
+      "name": "crush-v0.8.5-linux-x86_64",
+      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.5/crush-v0.8.5-linux-x86_64",
+      "size": 11322872
     },
     {
-      "name": "Crush_0.8.4_amd64.AppImage",
-      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.4/Crush_0.8.4_amd64.AppImage",
+      "name": "Crush_0.8.5_amd64.AppImage",
+      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.5/Crush_0.8.5_amd64.AppImage",
       "size": 88488440
     },
     {
-      "name": "Crush_0.8.4_amd64.deb",
-      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.4/Crush_0.8.4_amd64.deb",
-      "size": 12687676
+      "name": "Crush_0.8.5_amd64.deb",
+      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.5/Crush_0.8.5_amd64.deb",
+      "size": 12731852
+    },
+    {
+      "name": "Crush_0.8.5_x64-setup.exe",
+      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.5/Crush_0.8.5_x64-setup.exe",
+      "size": 8629597
+    },
+    {
+      "name": "Crush_0.8.5_x64_en-US.msi",
+      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.5/Crush_0.8.5_x64_en-US.msi",
+      "size": 11771904
     },
     {
       "name": "SHA256SUMS",
-      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.4/SHA256SUMS",
+      "url": "https://github.com/Chidi09/crush/releases/download/v0.8.5/SHA256SUMS",
       "size": 273
     }
   ],
